fcb077f12fcb45ba262b766d5be96a5f231a178d
EricBrown56 <groove505@gmail.com>
2024-11-12 15:14:26 -0600

    removed feedback comment about previous error

--- routes/feedbackBP.py
+++ routes/feedbackBP.py
@@ -5,7 +5,7 @@ feedback_blueprint = Blueprint('feedback_bp', __name__)
 
 #url_prefix='/feedback'
 
-feedback_blueprint.route('/', methods=['POST'])(create_feedback)# trying to implement the update functions. Also, the feedback output is a blank dictionary even though it creates it.
+feedback_blueprint.route('/', methods=['POST'])(create_feedback)# trying to implement the update functions.
 feedback_blueprint.route('/<feedback_id>', methods=['GET'])(get_feedback_by_id)
 feedback_blueprint.route('/task/<task_id>', methods=['GET'])(get_feedback_by_task_id)
 feedback_blueprint.route('/task_neighbor/<task_neighbor_id>', methods=['GET'])(get_feedback_by_task_neighbor_id)
