44e1f69cdbc80b740402273df1406031a3376a78
EricBrown56 <groove505@gmail.com>
2024-11-19 18:49:39 -0600

    tested all post funcions

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
 import os
 
+# class DevelopmentConfig:
+#     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+#     CACHE_TYPE = "SimpleCache"
+#     DEBUG = True
+
+# # to connect to your computer, after root:, you can put your password between the colon and @
+
+# class TestingConfig:
+#     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_SQLALCHEMY_DATABASE_URI')
+#     CACHE_TYPE = "SimpleCache"
+#     TESTING = True
+
 class DevelopmentConfig:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Groovin@localhost/LocalLynk'
     CACHE_TYPE = "SimpleCache"
     DEBUG = True
-
-# to connect to your computer, after root:, you can put your password between the colon and @
-
-class TestingConfig:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_SQLALCHEMY_DATABASE_URI')
-    CACHE_TYPE = "SimpleCache"
-    TESTING = True
