9b74c7e9c1100b9540bab396407dc5da2b75cbdc
EricBrown56 <groove505@gmail.com>
2024-11-20 21:37:54 -0600

    updated config.py file to connect to the postgresql database

--- config.py
+++ config.py
@@ -1,18 +1,20 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 class DevelopmentConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     CACHE_TYPE = "SimpleCache"
     DEBUG = True
 
-# to connect to your computer, after root:, you can put your password between the colon and @
+# # to connect to your computer, after root:, you can put your password between the colon and @
 
-class TestingConfig:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_SQLALCHEMY_DATABASE_URI')
-    CACHE_TYPE = "SimpleCache"
-    TESTING = True
+# class TestingConfig:
+#     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_SQLALCHEMY_DATABASE_URI')
+#     CACHE_TYPE = "SimpleCache"
+#     TESTING = True
 
-#class DevelopmentConfig:
-    #SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Groovin@localhost/#LocalLynk'
-    #CACHE_TYPE = "SimpleCache"
-    #DEBUG = True
+# class DevelopmentConfig:
+#     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Groovin@localhost/LocalLynk'
+#     CACHE_TYPE = "SimpleCache"
+#     DEBUG = True
