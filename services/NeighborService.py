d565b3bd1feccda7a9b4f9895d4b04b3ee4a036b
EricBrown56 <groove505@gmail.com>
2024-12-09 22:35:16 -0600

    t

--- services/NeighborService.py
+++ services/NeighborService.py
@@ -86,7 +86,7 @@ def get_all_neighbors(page=1, per_page=20):
 def get_neighbor_by_id(neighbor_id):
     query = select(Neighbor).where(Neighbor.id == neighbor_id)
     result = db.session.execute(query).one_or_none()
-    neighbor = result.scalars()
+    neighbor = result
     if not neighbor:
         raise ValueError("Neighbor not found.")
     return neighbor
