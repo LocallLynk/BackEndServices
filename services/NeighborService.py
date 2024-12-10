a7431014411ea9214b6a4fe169e1bf0cf3138a02
EricBrown56 <groove505@gmail.com>
2024-12-09 22:46:18 -0600

    testing

--- services/NeighborService.py
+++ services/NeighborService.py
@@ -93,7 +93,7 @@ def get_neighbor_by_id(neighbor_id):
 
 def get_neighbor_by_username(username):
     query = select(Neighbor).where(Neighbor.username == username)
-    result = db.session.execute(query).one_or_none()
+    result = db.session.execute(query)
     neighbor = result.scalars()
     if not neighbor:
         raise ValueError("Neighbor not found.")
@@ -101,7 +101,7 @@ def get_neighbor_by_username(username):
 
 def get_neighbor_by_email(email):
     query = select(Neighbor).where(Neighbor.email == email)
-    result = db.session.execute(query).one_or_none()
+    result = db.session.execute(query)
     neighbor = result.scalars()
     if not neighbor:
         raise ValueError("Neighbor not found.")
